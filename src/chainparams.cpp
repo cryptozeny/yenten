80a1ad7f020c7b84f734f0e08202dd0dba5e8a7b
yenten2 <36344285+yenten2@users.noreply.github.com>
2018-02-10 15:09:16 -0700

    Update chainparams.cpp
    added print statements so we can create new genesis block

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -47,7 +47,7 @@ public:
         //     CTxIn(COutPoint(000000, -1), coinbase 04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73)
         //     CTxOut(nValue=50.00000000, scriptPubKey=0x5F1DF16B2B704C8A578D0B)
         //   vMerkleTree: 4a5e1e
-        const char* pszTimestamp = "Tondemonee Atasya Kamisama Dayo...2017.10.03";
+        const char* pszTimestamp = "Bad news this season as flu cases near 2009 highs";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -57,12 +57,14 @@ public:
         genesis.vtx.push_back(txNew);
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+        printf("genesis.BuildMerkleTree = %s\n", genesis.BuildMerkleTree().ToString().c_str());
         genesis.nVersion = 1;
         genesis.nTime    = 1507032424;
         genesis.nBits    = 0x1e3fffff;
         genesis.nNonce   = 168808;
 
         hashGenesisBlock = genesis.GetHash();
+        printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
         assert(hashGenesisBlock == uint256("0x00001828d845205a951f9609e011775e035b00c7fb476310261ef30460cdccab"));
         assert(genesis.hashMerkleRoot == uint256("0x92874e7221a12cedde2a7335f733ee03f79694355cda3e383d3f531e3cf9672a"));
 
@@ -127,6 +129,7 @@ public:
         genesis.nTime = 1507032223;
         genesis.nNonce = 238202;
         hashGenesisBlock = genesis.GetHash();
+        printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
         assert(hashGenesisBlock == uint256("0x00002a66f0c687f619934409d376c52fe721833f05940e4c20deaaa0122d7fb8"));
 
         vFixedSeeds.clear();
@@ -161,6 +164,7 @@ public:
         genesis.nBits = 0x207fffff;
         genesis.nNonce = 1;
         hashGenesisBlock = genesis.GetHash();
+        printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
         nDefaultPort = 18432;
         strDataDir = "regtest";
         //printf("hashGenesisBlock %s\n", hashGenesisBlock.ToString().c_str());
