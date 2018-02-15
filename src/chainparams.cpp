6a9bb9008ee7b46ce2ece1271c27d94df1dbf0a5
yenten2 <36344285+yenten2@users.noreply.github.com>
2018-02-14 20:14:40 -0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -65,8 +65,8 @@ public:
 
         hashGenesisBlock = genesis.GetHash();
           printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
-#if 0
-        {
+//#if 0
+        //{
             printf("calc new genesis block\n");
             printf("hashMerkleRoot %s\n", genesis.hashMerkleRoot.ToString().c_str());
             printf("bnProofOfWorkLimit 0x%x\n", bnProofOfWorkLimit.GetCompact());
@@ -79,7 +79,7 @@ public:
 
             printf("hashGenesisBlock %s\n", hashGenesisBlock.ToString().c_str());
             printf("genesis.nNonce %d\n", genesis.nNonce);
-        }
+        //}
 #endif
         assert(hashGenesisBlock == uint256("0x9e7839a911f84e6485539ee6512f6ebaa376d0cad366aac4eccc794c2150b435"));  //assert(hashGenesisBlock == uint256("0x")); for generating genesis block
         assert(genesis.hashMerkleRoot == uint256("0x41a5cf0c21a365d33fe36e25fa2548b9d0d69f1036daab738fcccf51c3447d88"));  //assert(genesis.hashMerkleRoot == uint256("0x")); for generating genesis block
